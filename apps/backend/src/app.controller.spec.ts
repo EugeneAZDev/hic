74d4feee05e0f9f037377e5c5bca65b27727fbb9
EugeneAZDev <eugene.zolotkov@gmail.com>
2025-09-13 12:13:42 +0300

    ci: add test script and create simple tests for all services

---
+++ apps/backend/src/app.controller.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+
+describe("AppController", () => {
+  let controller: AppController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+    }).compile();
+
+    controller = module.get<AppController>(AppController);
+  });
+
+  it("should be defined", () => {
+    expect(controller).toBeDefined();
+  });
+
+  it("should return health status", () => {
+    const result = controller.health();
+    expect(result).toEqual({
+      status: "ok",
+      message: "Backend is running!",
+    });
+  });
+});
